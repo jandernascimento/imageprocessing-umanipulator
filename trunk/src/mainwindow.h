8d48e82923bd81dfed3a167e8ac93f90a06b9897
jbotnascimento <jbotnascimento@04d00976-414e-c661-e2ab-a3a293d51e39>
2011-01-04 19:33:09 +0000

    adding menu and updating status doc

--- trunk/src/mainwindow.h
+++ trunk/src/mainwindow.h
@@ -17,6 +17,16 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QMenu *filemenu;
+    QMenu *viewmenu;
+    QMenu *imagemenu;
+    QMenu *colormenu;
+    void setupFileMenu(QMenu *menu);
+    void setupViewMenu(QMenu *menu);
+    void setupImageMenu(QMenu *menu);
+    void setupImageFilterSubMenu(QMenu *menu);
+    void setupImageAdvancedSubMenu(QMenu *menu);
+    void setupColorMenu(QMenu *menu);
 };
 
 #endif // MAINWINDOW_H
