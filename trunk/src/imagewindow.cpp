6f26911f547aa371e586e778541ff92b62b5bd7c
jbotnascimento <jbotnascimento@04d00976-414e-c661-e2ab-a3a293d51e39>
2011-01-05 14:15:29 +0000

    adding code to change pixel color

--- trunk/src/imagewindow.cpp
+++ trunk/src/imagewindow.cpp
@@ -1,5 +1,6 @@
 #include "imagewindow.h"
 #include "ui_imagewindow.h"
+#include <QtCore/QtCore>
 
 ImageWindow::ImageWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -13,16 +14,25 @@ ImageWindow::ImageWindow(QWidget *parent, QString filesname) :
     ui(new Ui::ImageWindow)
 {
     ui->setupUi(this);
-    const QImage *qi=new QImage(filesname/*QString("/home/jander/Desktop/P1030833.JPG")*/,0);
+    const QImage *qi=new QImage(filesname,0);
 
-    //QPicture *p=new QPicture();
+    for(int x=0;x<qi->height();x++){
+
+        for(int y=0;y<qi->width();y++){
+
+            QRgb *pixel = (QRgb *)qi->scanLine(x);
+            pixel=(pixel+y);
+            //*pixel = qRgba(255,0,0,255);
+
+        }
+
+    }
+
+    //*pixel = qRgba(255,0,0,100);
+    //int blue = qBlue(*pixel);
+
     ui->label->setPixmap(QPixmap::fromImage(*qi,Qt::AutoColor));
 
-    //ui->verticalLayout->addWidget(qi);
-
-    //const QList ql=new QList();
-    //ql.append(qi);
-    //centralWidget()->children().append(ql);
 }
 
 ImageWindow::~ImageWindow()
