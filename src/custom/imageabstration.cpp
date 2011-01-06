f13592b18878d16458c8568c939137e235a86c15
jbotnascimento <jbotnascimento@04d00976-414e-c661-e2ab-a3a293d51e39>
2011-01-06 16:26:32 +0000

    adding methods to get min and max value of each color

--- src/custom/imageabstration.cpp
+++ src/custom/imageabstration.cpp
@@ -1,13 +1,38 @@
 #include "imageabstration.h"
 
 ImageAbstraction::ImageAbstraction(const QString &fileName, const char *format):QImage(fileName, format ){
+
     qDebug("Calling custom constructor");
+
+    redmax=-1;
+    redmin=300;
+    greenmax=-1;
+    greenmin=300;
+    bluemax=-1;
+    bluemin=300;
+
     for(int x=0;x<this->height();x++){
         for(int y=0;y<this->width();y++){
             //Sets all red values to the maximum, so the image becomes i little redy
             setPixel(ImageAbstraction::red,x,y,255);
+
+            int blue=qBlue(*getPixel(ImageAbstraction::blue,x,y));
+            if(blue>bluemax) bluemax=blue;
+            if(blue<bluemin) bluemin=blue;
+
+            int red=qRed(*getPixel(ImageAbstraction::red,x,y));
+            if(red>redmax) redmax=red;
+            if(red<redmin) redmin=red;
+
+            int green=qGreen(*getPixel(ImageAbstraction::green,x,y));
+            if(green>greenmax) greenmax=green;
+            if(green<greenmin) greenmin=green;
         }
     }
+
+    qDebug("Red min:%i max:%i",getMinColorValue(ImageAbstraction::red),getMaxColorValue(ImageAbstraction::red));
+    qDebug("Green min:%i max:%i",getMinColorValue(ImageAbstraction::green),getMaxColorValue(ImageAbstraction::green));
+    qDebug("Blue min:%i max:%i",getMinColorValue(ImageAbstraction::blue),getMaxColorValue(ImageAbstraction::blue));
 }
 
 QRgb* ImageAbstraction::getPixel(enum ecolor color, int x, int y){
@@ -30,4 +55,37 @@ QRgb* ImageAbstraction::setPixel(int x, int y,int red, int green, int blue){
     setPixel(ImageAbstraction::red,x,y,red);
     setPixel(ImageAbstraction::green,x,y,green);
     return setPixel(ImageAbstraction::blue,x,y,blue);
+}
+
+int ImageAbstraction::getMaxColorValue(enum ecolor color){
+    switch(color){
+        case ImageAbstraction::red:
+            return this->redmax;
+            break;
+
+        case ImageAbstraction::green:
+            return this->greenmax;
+            break;
+
+        case ImageAbstraction::blue:
+            return this->bluemax;
+            break;
+    };
+}
+
+int ImageAbstraction::getMinColorValue(enum ecolor color){
+
+    switch(color){
+        case ImageAbstraction::red:
+            return this->redmin;
+            break;
+
+        case ImageAbstraction::green:
+            return this->greenmin;
+            break;
+
+        case ImageAbstraction::blue:
+            return this->bluemin;
+            break;
+    };
 }
--- src/custom/imageabstration.h
+++ src/custom/imageabstration.h
@@ -4,15 +4,21 @@
 
 class ImageAbstraction : public QImage
 {
-
-   public:
-      enum ecolor {red, green, blue};
-      explicit ImageAbstraction(const QString &fileName, const char *format = 0);
-      QRgb* getPixel(enum ecolor color, int x, int y);
-      QRgb* setPixel(enum ecolor color, int x, int y,int value);
-      QRgb* setPixel(int x, int y,int red, int green, int blue);
-
-
+     public:
+          enum ecolor {red, green, blue};
+          explicit ImageAbstraction(const QString &fileName, const char *format = 0);
+          QRgb* getPixel(enum ecolor color, int x, int y);
+          QRgb* setPixel(enum ecolor color, int x, int y,int value);
+          QRgb* setPixel(int x, int y,int red, int green, int blue);
+          int getMinColorValue(enum ecolor color);
+          int getMaxColorValue(enum ecolor color);
+    private:
+          int redmax;
+          int redmin;
+          int greenmax;
+          int greenmin;
+          int bluemax;
+          int bluemin;
 };
 
 #endif // IMAGEABSTRATION_H
