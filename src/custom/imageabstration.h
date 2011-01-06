6907ed8f2eb13454375b88c1ffbcc16fa0725e73
jbotnascimento <jbotnascimento@04d00976-414e-c661-e2ab-a3a293d51e39>
2011-01-06 16:33:05 +0000

    eliminating unused method

--- src/custom/imageabstration.h
+++ src/custom/imageabstration.h
@@ -7,7 +7,7 @@ class ImageAbstraction : public QImage
      public:
           enum ecolor {red, green, blue};
           explicit ImageAbstraction(const QString &fileName, const char *format = 0);
-          QRgb* getPixel(enum ecolor color, int x, int y);
+          QRgb* getPixel(int x, int y);
           QRgb* setPixel(enum ecolor color, int x, int y,int value);
           QRgb* setPixel(int x, int y,int red, int green, int blue);
           int getMinColorValue(enum ecolor color);
