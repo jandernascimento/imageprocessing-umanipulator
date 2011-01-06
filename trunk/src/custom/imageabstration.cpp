c73352ae967f5f858646153688125b2d3d692b7f
jbotnascimento <jbotnascimento@04d00976-414e-c661-e2ab-a3a293d51e39>
2011-01-06 12:19:55 +0000

    adding methods for set pixel in image class

--- trunk/src/custom/imageabstration.cpp
+++ trunk/src/custom/imageabstration.cpp
@@ -1,58 +1,33 @@
 #include "imageabstration.h"
 
-ImageAbstraction::ImageAbstraction(QWidget* parent){
+ImageAbstraction::ImageAbstraction(const QString &fileName, const char *format):QImage(fileName, format ){
+    qDebug("Calling custom constructor");
+    for(int x=0;x<this->height();x++){
+        for(int y=0;y<this->width();y++){
+            //Sets all red values to the maximum, so the image becomes i little redy
+            setPixel(ImageAbstraction::red,x,y,255);
+        }
+    }
+}
+
+QRgb* ImageAbstraction::getPixel(enum ecolor color, int x, int y){
+    QRgb *pixel = (QRgb *)this->scanLine(x);
+    pixel=(pixel+y);
+    return pixel;
+}
+
+QRgb* ImageAbstraction::setPixel(enum ecolor color, int x, int y, int value){
+    QRgb *pixel = getPixel(color,x,y);
+    int red = color==ImageAbstraction::red?value:qRed(*pixel);
+    int green = color==ImageAbstraction::green?value:qGreen(*pixel);
+    int blue = color==ImageAbstraction::blue?value:qBlue(*pixel);
+    *pixel = qRgba(red,green,blue,255);
+    return pixel;
 
 }
 
-ImageAbstraction::ImageAbstraction(){
-
-    QImage();
-
-
-};
-
-ImageAbstraction::ImageAbstraction(const QSize &size, Format format){
-
-    QImage(size, format);
-
-};
-
-ImageAbstraction::ImageAbstraction(int width, int height, Format format){
-
-    QImage(width, height, format);
-
-
-};
-
-ImageAbstraction::ImageAbstraction(uchar *data, int width, int height, Format format){
-
-    QImage(data, width, height, format);
-
-
-};
-
-ImageAbstraction::ImageAbstraction(const uchar *data, int width, int height, Format format){
-
-    QImage(data, width, height, format);
-
-
-};
-
-ImageAbstraction::ImageAbstraction(uchar *data, int width, int height, int bytesPerLine, Format format){
-
-    QImage(data, width, height, bytesPerLine, format);
-
-
-};
-
-ImageAbstraction::ImageAbstraction(const uchar *data, int width, int height, int bytesPerLine, Format format){
-
-   QImage(data, width, height, bytesPerLine, format);
-
-};
-
-ImageAbstraction::ImageAbstraction(const QString &fileName, const char *format):QImage(fileName, format ){
-
-    qDebug("Calling custom constructor");
-
+QRgb* ImageAbstraction::setPixel(int x, int y,int red, int green, int blue){
+    setPixel(ImageAbstraction::red,x,y,red);
+    setPixel(ImageAbstraction::green,x,y,green);
+    return setPixel(ImageAbstraction::blue,x,y,blue);
 }
