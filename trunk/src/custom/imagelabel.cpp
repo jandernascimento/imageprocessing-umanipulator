2e8fa59c740a7b68ddd5729bd937a20383c6869b
jbotnascimento <jbotnascimento@04d00976-414e-c661-e2ab-a3a293d51e39>
2011-01-12 10:51:33 +0000

    removing permanent croping tool

--- trunk/src/custom/imagelabel.cpp
+++ trunk/src/custom/imagelabel.cpp
@@ -5,23 +5,29 @@
 ImageLabel::ImageLabel(QWidget *parent){
     this->setParent(parent);
     rubberBand = new QRubberBand(QRubberBand::Rectangle, this);
+    rubberbandactivated=false;
 
 }
 
 void ImageLabel::mouseMoveEvent(QMouseEvent *e) {
 
+    if(rubberbandactivated){
      rubberBand->setGeometry(QRect(start, e->pos()).normalized());
      rubberBand->show();
+   }
 
 }
 
 void ImageLabel::mousePressEvent (QMouseEvent* event)
 {
 
-    start=event->pos();
+    if(rubberbandactivated){
+        start=event->pos();
 
-    rubberBand->setGeometry(QRect(start, start));
-    rubberBand->show();
+        rubberBand->setGeometry(QRect(start, start));
+        rubberBand->show();
+
+    }
 
     if(event->button() == Qt::LeftButton)
     {
@@ -31,15 +37,20 @@ void ImageLabel::mousePressEvent (QMouseEvent* event)
 }
 
 void ImageLabel::mouseReleaseEvent ( QMouseEvent * event ){
-    end=event->pos();
 
-    if((end.x()-start.x())>0&&
-       (end.y()-start.y())>0){
-        emit areaselected(start.x(),start.y(),end.x(),end.y());
+    if(rubberbandactivated){
+        end=event->pos();
+
+        if((end.x()-start.x())>0&&
+           (end.y()-start.y())>0){
+            emit areaselected(start.x(),start.y(),end.x(),end.y());
+        }
+
+        rubberBand->hide();
     }
-
-    rubberBand->hide();
 
 }
 
-
+void ImageLabel::setRubberband(bool status){
+    rubberbandactivated=status;
+}
