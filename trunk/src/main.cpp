97b0c5592538c779899930858faf64a1eb00e980
jbotnascimento <jbotnascimento@04d00976-414e-c661-e2ab-a3a293d51e39>
2011-01-12 16:48:54 +0000

    adding toolbar

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,10 +1,16 @@
 //#include <QtGui/QApplication>
 #include "mainwindow.h"
-//#include "dialogcontrast.h"
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
+
+    //QDir::addResourceSearchPath("resources")
+
+    //Q_INIT_RESOURCE();
+
     MainWindow w;
     w.show();
 
