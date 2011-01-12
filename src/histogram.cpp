ca845f492f41594d4ffc33062dd789f8a818d4e5
raraujo.oliveira <raraujo.oliveira@04d00976-414e-c661-e2ab-a3a293d51e39>
2011-01-12 15:23:31 +0000

    implement method that return the max number of pixels for all the color's level

--- src/histogram.cpp
+++ src/histogram.cpp
@@ -17,9 +17,9 @@ Histogram::~Histogram()
     delete ui;
 }
 
-void Histogram::fillArray(int *n_colors,int n_items){
+void Histogram::fillArray(int *n_colors,int n_items,int maxy,int miny){
     for(int i=0;i<n_items;i++){
-        n_colors[i]=(rand()%(350-30));//350=maxy, 30=miny. generate a number between 0 and 349
+        n_colors[i]=(rand()%(maxy-miny));//generate a number between miny and maxy
         //qDebug("\n%i",n_colors[i]);
     }
 }
@@ -28,15 +28,17 @@ void Histogram::drawHistogram(void){
     int minx=30;
     int maxx=550; //x axis goes from 0 to 255. So maxx is equivalent to 255
     int miny=30;
-    int maxy=350;
+    int maxy=image->getMaxNumberColor();
+    qDebug("max:%i",maxy);
+
     int maxx_hist_default=255; //the histogram goes from 0 to 255, this starts in 30
-    int weight_hist_default=25; //the interval of histogram
+    int weight_hist_default=1; //the interval of histogram
     int weight_hist=(maxx-minx)*weight_hist_default/maxx_hist_default; //finding the interval's relation between 550 and 255
 
     //filling the array with the number os pixels for each color's level
     int n_columns=maxx_hist_default/weight_hist_default; //number of columns of the histogram
     int n_colors[n_columns]; //number of times that color's level appears (height of the column of the histogram). ||should be passed to the procedure by param
-    //fillArray(n_colors,n_columns); //fill the array with random values
+    //fillArray(n_colors,n_columns,maxy,miny); //fill the array with random values
     /*/
     for(int i=0;i<n_columns;i++){
         n_colors[i]=0;
@@ -52,9 +54,10 @@ void Histogram::drawHistogram(void){
 
     QGraphicsScene *scene=new QGraphicsScene();
     //x1,y1,x2,y2
-    scene->setSceneRect( 0, 0, 598, 398 ); //window
+    //scene->setSceneRect( 0, 0, 598, 398 ); //window
     scene->addLine(minx,miny,minx,maxy); //y axis
     scene->addLine(minx,maxy,maxx,maxy); //x axis
+
 
     //drawing the rectangles
     int actual_x=minx;
