b535c1d65439b5d326c5f0b2d84d6d137578ff86
raraujo.oliveira <raraujo.oliveira@04d00976-414e-c661-e2ab-a3a293d51e39>
2011-01-18 14:48:31 +0000

    restructure of the intelligent resizing functions

--- src/mainwindowactions.cpp
+++ src/mainwindowactions.cpp
@@ -6,106 +6,14 @@
 #include "dialogabout.h"
 #include "dialogsetkernel.h"
 #include "dialoglog.h"
+#include "intelligentresizing.cpp"
 
 void MainWindow::findPaths(void){
-    qDebug("heigth: %i, width: %i",image->height(),image->width());
-
-    int size=(image->height())*(image->width());
-    int * energy_matrix=(int *) malloc(sizeof(int) * size);//image->height*image->width));
-
-    //reference: http://en.wikipedia.org/wiki/Seam_carving
-    //first, find vertical seams
-
-    //first line:
-    for(int col = 0; col < image->width(); col++) {
-        int lin=0;
-        int current_pixel   = image->getPixelColorIntensity(ImageAbstraction::blue,col,lin);
-        energy_matrix[image->width()*lin+col]=current_pixel;
-    }
-    //first col:
-    for(int lin = 1; lin < image->height(); lin++) {
-        int col=0;
-        int current_pixel   = image->getPixelColorIntensity(ImageAbstraction::blue,col,lin);
-        int neighbor_pixel2 = image->getPixelColorIntensity(ImageAbstraction::blue,col,lin-1);
-        int neighbor_pixel3 = image->getPixelColorIntensity(ImageAbstraction::blue,col+1,lin-1);
-        energy_matrix[image->width()*lin+col]=findMinValue(current_pixel+neighbor_pixel2,
-                                                           current_pixel+neighbor_pixel2,
-                                                           current_pixel+neighbor_pixel3);
-    }
-    //last col:
-    for(int lin = 1; lin < image->height(); lin++) {
-        int col=image->width()-1;
-        int current_pixel   = image->getPixelColorIntensity(ImageAbstraction::blue,col,lin);
-        int neighbor_pixel1 = image->getPixelColorIntensity(ImageAbstraction::blue,col-1,lin-1);
-        int neighbor_pixel2 = image->getPixelColorIntensity(ImageAbstraction::blue,col,lin-1);
-        energy_matrix[image->width()*lin+col]=findMinValue(current_pixel+neighbor_pixel1,
-                                                           current_pixel+neighbor_pixel2,
-                                                           current_pixel+neighbor_pixel2);
-    }
-    //next lines:
-    for (int lin = 1; lin < image->height(); lin++)
-        for (int col = 0; col < image->width(); col++) {
-            int current_pixel   = image->getPixelColorIntensity(ImageAbstraction::blue,col,lin);
-            int neighbor_pixel1 = image->getPixelColorIntensity(ImageAbstraction::blue,col-1,lin-1);
-            int neighbor_pixel2 = image->getPixelColorIntensity(ImageAbstraction::blue,col,lin-1);
-            int neighbor_pixel3 = image->getPixelColorIntensity(ImageAbstraction::blue,col+1,lin-1);
-            energy_matrix[image->width()*lin+col]=findMinValue(current_pixel+neighbor_pixel1,
-                                                               current_pixel+neighbor_pixel2,
-                                                               current_pixel+neighbor_pixel3);
-
-        }
-
-    free(energy_matrix);
-    qDebug("finished");
-}
-
-int MainWindow::findMinValue(int value1,int value2, int value3){
-    if (value1>=value2 && value1>=value3)
-        return value1;
-    else if (value2>=value3)
-        return value2;
-    else
-        return value3;
+    findPaths2();
 }
 
 void MainWindow::detectEdges(void){
-
-    int size=3;
-    double *gx=(double *)malloc(sizeof(double)*(size*size));
-    double *gy=(double *)malloc(sizeof(double)*(size*size));
-
-    //[line+col]
-    gx[size*0+0]=-1;  gx[size*0+1]=0;  gx[size*0+2]=1;
-    gx[size*1+0]=-2;  gx[size*1+1]=0;  gx[size*1+2]=2;
-    gx[size*2+0]=-1;  gx[size*2+1]=0;  gx[size*2+2]=1;
-
-    gy[size*0+0]= 1; gy[size*0+1]= 2; gy[size*0+2]= 1;
-    gy[size*1+0]= 0; gy[size*1+1]= 0; gy[size*1+2]= 0;
-    gy[size*2+0]=-1; gy[size*2+1]=-2; gy[size*2+2]=-1;
-
-    image->ApplyConvolution(size,gx,'c'); //this 'c' does not matter
-    image->ApplyConvolution(size,gy,'c');
-
-    for (int j = 0; j < image->height(); j++) {
-      for (int i = 0; i < image->width(); i++) {
-
-          QRgb *pixel=image->getPixel(j,i);
-
-          int col=image->getPixelColorIntensity(ImageAbstraction::blue,j,i);
-
-          if(col<122 || col >129)
-            *pixel=qRgba(255,255,255,255);
-          else
-            *pixel=qRgba(0,0,0,255);
-
-      }
-    }
-
-    free(gy);
-    free(gx);
-
-    //updating the image in the interface
-    label->setPixmap(QPixmap::fromImage(*this->image,Qt::AutoColor));
+    detectEdges2();
 }
 
 void MainWindow::histogram(void){
